c8b155f9e19576d150663fdcc3782ba74ef3c93e
Willem Liu <willem.liu@gmail.com>
2011-11-06 01:12:55 +0100

    Added turkish translation.

--- src/qml-translations.tr.ts
+++ src/qml-translations.tr.ts
@@ -6,37 +6,37 @@
     <message>
         <location filename="AboutPage.qml" line="31"/>
         <source>EasyList - About</source>
-        <translation type="unfinished"></translation>
+        <translation>KolayListe - Hakkında</translation>
     </message>
     <message>
         <location filename="AboutPage.qml" line="64"/>
         <source>Created with Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>Qt ile yaratılmıştır</translation>
     </message>
     <message>
         <location filename="AboutPage.qml" line="73"/>
         <source>Created by &lt;a href=&apos;http://willemliu.nl/donate/&apos;&gt;Willem Liu&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;a href=&apos;http://willemliu.nl/donate/&apos;&gt;Willem Liu&lt;/a&gt; tarafından yaratılmıştır</translation>
     </message>
     <message>
         <location filename="AboutPage.qml" line="83"/>
         <source>Drop tables?</source>
-        <translation type="unfinished"></translation>
+        <translation>Tablolar kaldırılsın mı?</translation>
     </message>
     <message>
         <location filename="AboutPage.qml" line="84"/>
         <source>Do you really want to remove all tables used by EasyList?</source>
-        <translation type="unfinished"></translation>
+        <translation>KolayListe tarafından kullanılan tüm tabloları silmek istediğinize emin misiniz?</translation>
     </message>
     <message>
         <location filename="AboutPage.qml" line="85"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>Tamam</translation>
     </message>
     <message>
         <location filename="AboutPage.qml" line="86"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>İptal</translation>
     </message>
 </context>
 <context>
@@ -44,17 +44,17 @@
     <message>
         <location filename="EditPage.qml" line="9"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaydet</translation>
     </message>
     <message>
         <location filename="EditPage.qml" line="10"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>İptal</translation>
     </message>
     <message>
         <location filename="EditPage.qml" line="15"/>
         <source>Paste</source>
-        <translation type="unfinished"></translation>
+        <translation>Yapıştır</translation>
     </message>
     <message>
         <location filename="EditPage.qml" line="46"/>
@@ -64,7 +64,12 @@ a new item.
 
 Tip: You can copy text from other apps
 and paste it here as well.</source>
-        <translation type="unfinished"></translation>
+        <translation>Listenizi yaratmak için metin girin
+Her yeni satır ve virgül
+yeni bir öğe oluşturur.
+
+İpucu: Diğer uygulamalardan metin
+kopyalayıp buraya yapıştırabilirsiniz.</translation>
     </message>
 </context>
 <context>
@@ -72,68 +77,68 @@ and paste it here as well.</source>
     <message>
         <location filename="ListsPage.qml" line="40"/>
         <source>EasyList - Lists</source>
-        <translation type="unfinished"></translation>
+        <translation>KolayListe - Listeler</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="133"/>
         <location filename="ListsPage.qml" line="253"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Ad değiştir</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="140"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaldır</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="152"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ayarlar</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="158"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Hakkında</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="174"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaydet</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="175"/>
         <location filename="ListsPage.qml" line="254"/>
         <location filename="ListsPage.qml" line="381"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>İptal</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="201"/>
         <location filename="ListsPage.qml" line="280"/>
         <source>New list name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Yeni liste adı:</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="378"/>
         <source>Remove list?</source>
-        <translation type="unfinished"></translation>
+        <translation>Liste kaldırılsın mı?</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="379"/>
         <source>Do you really want to remove [</source>
         <translatorcomment>This is part of the sentence: &quot;Do you really want to remove [listname] and all its items?&quot;</translatorcomment>
-        <translation type="unfinished"></translation>
+        <translation>[</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="379"/>
         <source>] and all its items?</source>
         <translatorcomment>This is part of the sentence: &quot;Do you really want to remove [listname] and all its items?&quot;</translatorcomment>
-        <translation type="unfinished"></translation>
+        <translation>] ve tüm öğelerini kaldırmak istediğinize emin misiniz?</translation>
     </message>
     <message>
         <location filename="ListsPage.qml" line="380"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>Tamam</translation>
     </message>
 </context>
 <context>
@@ -141,77 +146,79 @@ and paste it here as well.</source>
     <message>
         <location filename="MainPage.qml" line="65"/>
         <source>Deselect all</source>
-        <translation type="unfinished"></translation>
+        <translation>Tüm seçimleri kaldır</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="72"/>
         <source>Remove checked</source>
-        <translation type="unfinished"></translation>
+        <translation>İşaretli olanları kaldır</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="78"/>
         <source>Synchronise</source>
-        <translation type="unfinished"></translation>
+        <translation>Senkronize et</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="84"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ayarlar</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="90"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Hakkında</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="100"/>
         <source>Remove checked items?</source>
-        <translation type="unfinished"></translation>
+        <translation>İşaretli öğeler kaldırılsın mı?</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="101"/>
         <source>Do you really want to remove all checked items?</source>
-        <translation type="unfinished"></translation>
+        <translation>İşaretli tüm öğreleri kaldırmak istediğinize emin misiniz?</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="102"/>
         <location filename="MainPage.qml" line="113"/>
         <location filename="MainPage.qml" line="124"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>Tamam</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="103"/>
         <location filename="MainPage.qml" line="114"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>İptal</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="111"/>
         <source>Sync with online list?</source>
-        <translation type="unfinished"></translation>
+        <translation>Online liste ile senkronize edilsin mi?</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="112"/>
         <source>Do you really want sync your current list with the online list?
 
 All your current items will be overwritten.</source>
-        <translation type="unfinished"></translation>
+        <translation>Listenizi online liste ile senkronize etmek istediğinize emin misiniz?
+
+Varolan öğelerinizin üzerine yazılacak.</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="122"/>
         <source>Can&apos;t synchronize</source>
-        <translation type="unfinished"></translation>
+        <translation>Senkronize edilemiyor</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="123"/>
         <source>Unable to synchronize with online list. Please make sure you&apos;ve correctly setup your sync account in settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Online liste ile senkronizasyon yapılamıyor. Lütfen ayarlarda senkronizasyon hesabınızı doğru kurduğunuza emin olunuz.</translation>
     </message>
     <message>
         <location filename="MainPage.qml" line="245"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaldır</translation>
     </message>
 </context>
 <context>
@@ -219,87 +226,87 @@ All your current items will be overwritten.</source>
     <message>
         <location filename="SettingsPage.qml" line="50"/>
         <source>EasyList - Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>KolayListe - Ayarlar</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="66"/>
         <source>Themes</source>
-        <translation type="unfinished"></translation>
+        <translation>Temalar</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="132"/>
         <source>Sort</source>
-        <translation type="unfinished"></translation>
+        <translation>Sıralama</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="143"/>
         <source>Alphabetically:</source>
-        <translation type="unfinished"></translation>
+        <translation>Alfabetik olarak:</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="165"/>
         <source>Checked items to bottom:</source>
-        <translation type="unfinished"></translation>
+        <translation>İşaretli öğeler aşağıya:</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="195"/>
         <source>Orientation</source>
-        <translation type="unfinished"></translation>
+        <translation>Görünüm düzeni</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="214"/>
         <source>Portrait</source>
-        <translation type="unfinished"></translation>
+        <translation>Dikey</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="235"/>
         <source>Landscape</source>
-        <translation type="unfinished"></translation>
+        <translation>Yatay</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="256"/>
         <source>Auto</source>
-        <translation type="unfinished"></translation>
+        <translation>Otomatik</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="290"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="307"/>
         <source>Select Theme...</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema seç...</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="325"/>
         <source>Synchronise</source>
-        <translation type="unfinished"></translation>
+        <translation>Senkronize et</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="336"/>
         <source>Source:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaynak:</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="359"/>
         <source>Username:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı adı:</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="382"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifre:</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="404"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaydet</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="424"/>
         <source>Synchronization</source>
-        <translation type="unfinished"></translation>
+        <translation>Senkronizasyon</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="425"/>
@@ -307,12 +314,15 @@ All your current items will be overwritten.</source>
 
 You can create your online account on http://easylist.willemliu.nl.
 Default sync URL is: http://easylist.willemliu.nl/getList.php</source>
-        <translation type="unfinished"></translation>
+        <translation>Senkronizasyon hesabınızı kurduktan sonra listenizi online listenizle senkronize edebileceksiniz.
+
+Online hesabınızı http://easylist.willemliu.nl adresinde yaratabilirsiniz.
+Varsayılan senkronizasyon adresi: http://easylist.willemliu.nl/getList.php</translation>
     </message>
     <message>
         <location filename="SettingsPage.qml" line="426"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>Tamam</translation>
     </message>
 </context>
 </TS>
