84c546ab92d3bb2a506d1acacae9c735349a1241
Willem Liu <willem.liu@gmail.com>
2011-11-08 19:28:07 +0100

    Added russian translation file.

---
+++ src/qml-translations.ru.ts
@@ -0,0 +1,271 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>EasyList - About</source>
+        <translation>EasyList - О программе</translation>
+    </message>
+    <message>
+        <source>Created with Qt</source>
+        <translation>Сделано на Qt</translation>
+    </message>
+    <message>
+        <source>Created by &lt;a href=&apos;http://willemliu.nl/donate/&apos;&gt;Willem Liu&lt;/a&gt;</source>
+        <translation type="obsolete">Автор &lt;a href=&apos;http://willemliu.nl/donate/&apos;&gt;Willem Liu&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>Drop tables?</source>
+        <translation>Удалить таблицы?</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove all tables used by EasyList?</source>
+        <translation>Вы действительно хотите удалить данные используемые EasyList?</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>ОК</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <source>Created by &lt;a href=&apos;http://willemliu.nl/donate/&apos;&gt;Willem Liu&lt;/a&gt;
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Thanks to:</source>
+        <translation>Благодаря:</translation>
+    </message>
+</context>
+<context>
+    <name>EditPage</name>
+    <message>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Вставка</translation>
+    </message>
+    <message>
+        <source>Enter text to create your list.
+Each new line or comma delimits
+a new item.
+
+Tip: You can copy text from other apps
+and paste it here as well.</source>
+        <translation>Для создания списка вставьте текст. 
+Разделители-конец строки или запятая.
+
+Совет: Скопируйте текст из других 
+приложений и вставьте здесь.</translation>
+    </message>
+</context>
+<context>
+    <name>ListsPage</name>
+    <message>
+        <source>EasyList - Lists</source>
+        <translation>EasyList - Списки</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Переименовать</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <source>New list name:</source>
+        <translation>Новый список:</translation>
+    </message>
+    <message>
+        <source>Remove list?</source>
+        <translation>Удалить список?</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove [</source>
+        <translation>Вы действительно хотите удалить список [</translation>
+    </message>
+    <message>
+        <source>] and all its items?</source>
+        <translation>], включая его содержимое?</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>ОК</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Deselect all</source>
+        <translation>Сбросить отметки</translation>
+    </message>
+    <message>
+        <source>Remove checked</source>
+        <translation>Удалить отмеченные</translation>
+    </message>
+    <message>
+        <source>Synchronise</source>
+        <translation>Получить список с сервера</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>Remove checked items?</source>
+        <translation>Удалить отмеченные?</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove all checked items?</source>
+        <translation>Вы действительно хотите удалить все отмеченные строчки?</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>ОК</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <source>Sync with online list?</source>
+        <translation>Обновление списка</translation>
+    </message>
+    <message>
+        <source>Do you really want sync your current list with the online list?
+
+All your current items will be overwritten.</source>
+        <translation>Вы действительно хотите обновить список с сервера?
+
+Все текущие строчки списка будут потеряны.</translation>
+    </message>
+    <message>
+        <source>Can&apos;t synchronize</source>
+        <translation>Не удалось обновить список</translation>
+    </message>
+    <message>
+        <source>Unable to synchronize with online list. Please make sure you&apos;ve correctly setup your sync account in settings.</source>
+        <translation>Не удалось обновить с сервера. Удостоверьтесь, что в настройках правильно указаны параметры учетной записи.</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>EasyList - Settings</source>
+        <translation>EasyList - Настройки</translation>
+    </message>
+    <message>
+        <source>Themes</source>
+        <translation>Темы</translation>
+    </message>
+    <message>
+        <source>Sort</source>
+        <translation>Сортировать</translation>
+    </message>
+    <message>
+        <source>Alphabetically:</source>
+        <translation>По алфавиту:</translation>
+    </message>
+    <message>
+        <source>Checked items to bottom:</source>
+        <translation>Отмеченные строчки в конец:</translation>
+    </message>
+    <message>
+        <source>Orientation</source>
+        <translation>Ориентация</translation>
+    </message>
+    <message>
+        <source>Portrait</source>
+        <translation>Портрет</translation>
+    </message>
+    <message>
+        <source>Landscape</source>
+        <translation>Пейзаж</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Авто</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <source>Select Theme...</source>
+        <translation>Выбрать Тему…</translation>
+    </message>
+    <message>
+        <source>Synchronise</source>
+        <translation>Синхронизировать</translation>
+    </message>
+    <message>
+        <source>Source:</source>
+        <translation>Откуда:</translation>
+    </message>
+    <message>
+        <source>Username:</source>
+        <translation>Имя пользователя:</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <source>Synchronization</source>
+        <translation>Синхронизация</translation>
+    </message>
+    <message>
+        <source>When you&apos;ve setup your synchronization account then you&apos;ll be able to synchronize your list with your online list.
+
+You can create your online account on http://easylist.willemliu.nl.
+Default sync URL is: http://easylist.willemliu.nl/getList.php</source>
+        <translation>Когда вы настроите учетную запись вы сможете обновлять ваш список с сервера.
+
+Вы сможете создать Вашу учетную запись на сайте http://easylist.willemliu.nl.
+Адрес на сервере по умолчанию: http://easylist.willemliu.nl/getList.php</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>ОК</translation>
+    </message>
+</context>
+</TS>
