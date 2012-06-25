3506ceadd4fbe60b05eb1051920474cc04d91b3a
Willem Liu <willem.liu@gmail.com>
2012-06-25 21:53:09 +0200

    Added Hebrew language file.

---
+++ qml/easylist/qml-translations.he.ts
@@ -0,0 +1,275 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="he_IL">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>EasyList - About</source>
+        <translation>EasyList - אודות</translation>
+    </message>
+    <message>
+        <source>Created with Qt</source>
+        <translation>נוצר עם Qt</translation>
+    </message>
+    <message>
+        <source>Created by &lt;a href=&apos;http://willemliu.nl/donate/&apos;&gt;Willem Liu&lt;/a&gt;</source>
+        <translation>יוצר &lt;a href=&apos;http://willemliu.nl/donate/&apos;&gt;Willem Liu&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>Thanks to:</source>
+        <translation>תודות:</translation>
+    </message>
+    <message>
+        <source>Drop tables?</source>
+        <translation>למחוק טבלאות?</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove all tables used by EasyList?</source>
+        <translation>האם אתה באמת רוצה למחוק את כל הטבלאות ש-EasyList משתמש?</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>כן</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>ביטול</translation>
+    </message>
+</context>
+<context>
+    <name>EditPage</name>
+    <message>
+        <source>Save</source>
+        <translation>לשמור</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>ביטול</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>הדבק</translation>
+    </message>
+    <message>
+        <source>Enter text to create your list.
+Each new line or comma delimits
+a new item.
+
+Tip: You can copy text from other apps
+and paste it here as well.</source>
+        <translation>הזן טקסט כדי ליצור את הרשימה שלך.
+כל שורה חדש או פסיק
+תיצר פריט חדש.
+
+טיפ: ניתן להעתיק טקסט מיישומים אחרים
+ולהדביק אותו גם כאן.</translation>
+    </message>
+</context>
+<context>
+    <name>ListsPage</name>
+    <message>
+        <source>EasyList - Lists</source>
+        <translation>EasyList - רשימות</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>שינוי שם</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>למחוק</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>הגדרות</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>אודות</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>לשמור</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>ביטול</translation>
+    </message>
+    <message>
+        <source>New list name:</source>
+        <translation>רשימה חדשה:</translation>
+    </message>
+    <message>
+        <source>Remove list?</source>
+        <translation>למחוק רשימה?</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove [</source>
+        <translation>אתה בטוח רוצה למחוק רשימה [</translation>
+    </message>
+    <message>
+        <source>] and all its items?</source>
+        <translation>], כולל כל הפריטים?</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>כן</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Deselect all</source>
+        <translation>הסר בחירה מהכל</translation>
+    </message>
+    <message>
+        <source>Remove checked</source>
+        <translation>למחוק מסומנים</translation>
+    </message>
+    <message>
+        <source>Synchronise</source>
+        <translation>לסנכרן</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>הגדרות</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>אודות</translation>
+    </message>
+    <message>
+        <source>Remove checked items?</source>
+        <translation>למחוק מסומנים?</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove all checked items?</source>
+        <translation>האם אתה באמת רוצה להסיר את כל הפריטים המסומנים?</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>כן</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>ביטול</translation>
+    </message>
+    <message>
+        <source>Sync with online list?</source>
+        <translation>סינכרון רשימה?</translation>
+    </message>
+    <message>
+        <source>Do you really want sync your current list with the online list?
+
+All your current items will be overwritten.</source>
+        <translation>האם אתה באמת רוצה לסנכרן את הרשימה הנוכחית שלך עם הרשימה המקוונת?
+
+כל הפריטים הנוכחיות יוחלפו.</translation>
+    </message>
+    <message>
+        <source>Can&apos;t synchronize</source>
+        <translation>לא מצליח לסנכרן רשימה</translation>
+    </message>
+    <message>
+        <source>Unable to synchronize with online list. Please make sure you&apos;ve correctly setup your sync account in settings.</source>
+        <translation>לא מצליח לסנכרן עם הרשימה המקוונת. אנא ודא שהגדרת נכון את החשבון הסנכרון שלך בגדרות. </translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>למחוק</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>EasyList - Settings</source>
+        <translation>EasyList - הגדרות</translation>
+    </message>
+    <message>
+        <source>Themes</source>
+        <translation>ערכות נושא</translation>
+    </message>
+    <message>
+        <source>Sort</source>
+        <translation>לחלק</translation>
+    </message>
+    <message>
+        <source>Alphabetically:</source>
+        <translation>בסדר אלפביתי:</translation>
+    </message>
+    <message>
+        <source>Checked items to bottom:</source>
+        <translation>שורות מסומנות לסוף:</translation>
+    </message>
+    <message>
+        <source>Orientation</source>
+        <translation>התמצאות</translation>
+    </message>
+    <message>
+        <source>Portrait</source>
+        <translation>מצב אנכי</translation>
+    </message>
+    <message>
+        <source>Landscape</source>
+        <translation>מצב אופקי</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>אוטומטי</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation>ערכת נושא</translation>
+    </message>
+    <message>
+        <source>Select Theme...</source>
+        <translation>בחר ערכת נושא...</translation>
+    </message>
+    <message>
+        <source>Synchronise</source>
+        <translation>לסנכרן</translation>
+    </message>
+    <message>
+        <source>Source:</source>
+        <translation>מקור:</translation>
+    </message>
+    <message>
+        <source>Username:</source>
+        <translation>שם משתמש:</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>סיסמה:</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>לשמור</translation>
+    </message>
+    <message>
+        <source>Synchronization</source>
+        <translation>סנכרון</translation>
+    </message>
+    <message>
+        <source>When you&apos;ve setup your synchronization account then you&apos;ll be able to synchronize your list with your online list.
+
+You can create your online account on http://easylist.willemliu.nl.
+Default sync URL is: http://easylist.willemliu.nl/getList.php</source>
+        <translation>כאשר תגדיר חשבון הסנכרון שלך אז תוכל לסנכרן את הרשימה עם הרשימה המקוונת.
+
+באפשרותך ליצור חשבון אונליין ב http://easylist.willemliu.nl.
+כתובת סנכרון ברירת המחדל היא: http://easylist.willemliu.nl/getList.php</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>כן</translation>
+    </message>
+    <message>
+        <source>Comma delimited:</source>
+        <translation>עם פסיק</translation>
+    </message>
+    <message>
+        <source>Delimit by</source>
+        <translation>מחולקים</translation>
+    </message>
+</context>
+</TS>
